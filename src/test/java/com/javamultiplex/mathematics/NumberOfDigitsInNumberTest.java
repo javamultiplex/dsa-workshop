bdca9a103b4f6457f5f8fc7a212095cb79d16405
Rohit Agarwal <programmingwithrohit@gmail.com>
2020-12-17 22:10:48 +0530

    Added more test cases

--- src/test/java/com/javamultiplex/mathematics/NumberOfDigitsInNumberTest.java
+++ src/test/java/com/javamultiplex/mathematics/NumberOfDigitsInNumberTest.java
@@ -3,6 +3,8 @@ package com.javamultiplex.mathematics;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.math.BigInteger;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -28,6 +30,22 @@ public class NumberOfDigitsInNumberTest {
     }
 
     @Test
+    public void shouldTestMethod1WithBigInteger(){
+        int result1 = NumberOfDigitsInNumber.method1(BigInteger.valueOf(2020));
+        assertEquals(4, result1);
+        int result2 = NumberOfDigitsInNumber.method1(BigInteger.valueOf(1567));
+        assertEquals(4, result2);
+        int result3 = NumberOfDigitsInNumber.method1(BigInteger.valueOf(267));
+        assertEquals(3, result3);
+        int result4 = NumberOfDigitsInNumber.method1(BigInteger.valueOf(58964));
+        assertEquals(5, result4);
+        int result5 = NumberOfDigitsInNumber.method2(BigInteger.valueOf(1000));
+        assertEquals(4, result5);
+        int result6 = NumberOfDigitsInNumber.method2(BigInteger.valueOf(101));
+        assertEquals(3, result6);
+    }
+
+    @Test
     public void shouldTestMethod2(){
         int result1 = NumberOfDigitsInNumber.method2(2020);
         assertEquals(4, result1);
@@ -42,4 +60,22 @@ public class NumberOfDigitsInNumberTest {
         int result6 = NumberOfDigitsInNumber.method2(101);
         assertEquals(3, result6);
     }
+
+    @Test
+    public void shouldTestMethod2WithBigInteger(){
+        int result1 = NumberOfDigitsInNumber.method2(BigInteger.valueOf(2020));
+        assertEquals(4, result1);
+        int result2 = NumberOfDigitsInNumber.method2(BigInteger.valueOf(1567));
+        assertEquals(4, result2);
+        int result3 = NumberOfDigitsInNumber.method2(BigInteger.valueOf(267));
+        assertEquals(3, result3);
+        int result4 = NumberOfDigitsInNumber.method2(BigInteger.valueOf(58964));
+        assertEquals(5, result4);
+        int result5 = NumberOfDigitsInNumber.method2(BigInteger.valueOf(1000));
+        assertEquals(4, result5);
+        int result6 = NumberOfDigitsInNumber.method2(BigInteger.valueOf(101));
+        assertEquals(3, result6);
+    }
+
+
 }
