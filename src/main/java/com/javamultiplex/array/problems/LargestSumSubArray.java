90aecef24160134d31a36479e191c1a6e13ad6ed
Rohit Agarwal <programmingwithrohit@gmail.com>
2021-01-07 00:03:57 +0530

    Added another problem

---
+++ src/main/java/com/javamultiplex/array/problems/LargestSumSubArray.java
@@ -0,0 +1,23 @@
+package com.javamultiplex.array.problems;
+
+/**
+ * @author Rohit Agarwal on 06/01/21 11:52 pm
+ * @copyright www.javamultiplex.com
+ */
+public class LargestSumSubArray {
+
+    //Time - O(n*n), Space - O(1)
+    public static int method1(int[] arr, int n) {
+        int sum, max = 0;
+        for (int i = 0; i < n; i++) {
+            sum = arr[i];
+            for (int j = i + 1; j < n; j++) {
+                sum += arr[j];
+                if (sum > max) {
+                    max = sum;
+                }
+            }
+        }
+        return max;
+    }
+}
---
+++ src/test/java/com/javamultiplex/array/problems/LargestSumSubArrayTest.java
@@ -0,0 +1,19 @@
+package com.javamultiplex.array.problems;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author Rohit Agarwal on 06/01/21 11:58 pm
+ * @copyright www.javamultiplex.com
+ */
+public class LargestSumSubArrayTest {
+
+    @Test
+    public void shouldGetLargestSumSubArray_method1(){
+        int result = LargestSumSubArray.method1(new int[]{-3, 4, -1, -2, 1, 5}, 6);
+        assertEquals(7, result);
+    }
+}
