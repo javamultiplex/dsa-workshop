846ce536ae77a26fb9194caae35d6ee679ce007a
Rohit Agarwal <programmingwithrohit@gmail.com>
2021-02-26 00:09:01 +0530

    Added more methods ins matrix rotation

--- src/main/java/com/javamultiplex/matrix/RotateMatrix90Degree.java
+++ src/main/java/com/javamultiplex/matrix/RotateMatrix90Degree.java
@@ -19,6 +19,37 @@ public class RotateMatrix90Degree {
     }
 
     //Time - O(n*n), Space - O(n*n)
+    public static int[][] antiClockWiseMethod2(int[][] mat) {
+        int n = mat.length;
+        int[][] transpose = Matrix.transpose(mat);
+        int top, bottom, temp;
+        for (int j = 0; j < n; j++) {
+            top = 0;
+            bottom = n - 1;
+            while (top < bottom) {
+                temp = transpose[top][j];
+                transpose[top][j] = transpose[bottom][j];
+                transpose[bottom][j] = temp;
+                top++;
+                bottom--;
+            }
+        }
+        return transpose;
+    }
+
+    //Time - O(n*n), Space - O(1)
+    public static void antiClockWiseMethod3(int[][] mat) {
+        int n = mat.length;
+        int left = 0;
+        int top = 0;
+        int bottom = n - 1;
+        int right = n - 1;
+
+
+    }
+
+
+    //Time - O(n*n), Space - O(n*n)
     public static int[][] clockWiseMethod1(int[][] mat) {
         int n = mat.length;
         int[][] output = new int[n][n];
@@ -28,6 +59,25 @@ public class RotateMatrix90Degree {
             }
         }
         return output;
+    }
+
+    //Time - O(n*n), Space - O(n*n)
+    public static int[][] clockWiseMethod2(int[][] mat) {
+        int n = mat.length;
+        int[][] transpose = Matrix.transpose(mat);
+        int left, right, temp;
+        for (int i = 0; i < n; i++) {
+            left = 0;
+            right = n - 1;
+            while (left < right) {
+                temp = transpose[i][left];
+                transpose[i][left] = transpose[i][right];
+                transpose[i][right] = temp;
+                left++;
+                right--;
+            }
+        }
+        return transpose;
     }
 
 }
--- src/test/java/com/javamultiplex/matrix/RotateMatrix90DegreeTest.java
+++ src/test/java/com/javamultiplex/matrix/RotateMatrix90DegreeTest.java
@@ -32,6 +32,27 @@ public class RotateMatrix90DegreeTest {
     }
 
     @Test
+    public void shouldRotateAntiClockWise_method2() {
+        assertArrayEquals(new int[][]{
+                {3, 6, 9},
+                {2, 5, 8},
+                {1, 4, 7}
+        }, RotateMatrix90Degree.antiClockWiseMethod2(new int[][]{
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        }));
+
+        assertArrayEquals(new int[][]{
+                {2, 4},
+                {1, 3}
+        }, RotateMatrix90Degree.antiClockWiseMethod2(new int[][]{
+                {1, 2},
+                {3, 4}
+        }));
+    }
+
+    @Test
     public void shouldRotateClockWise_method1() {
         assertArrayEquals(new int[][]{
                 {7, 4, 1},
@@ -57,6 +78,39 @@ public class RotateMatrix90DegreeTest {
                 {9, 10, 11, 12},
                 {13, 14, 12, 16}
         }, RotateMatrix90Degree.clockWiseMethod1(new int[][]{
+                {4, 8, 12, 16},
+                {3, 7, 11, 12},
+                {2, 6, 10, 14},
+                {1, 5, 9, 13}
+        }));
+    }
+
+    @Test
+    public void shouldRotateClockWise_method2() {
+        assertArrayEquals(new int[][]{
+                {7, 4, 1},
+                {8, 5, 2},
+                {9, 6, 3}
+        }, RotateMatrix90Degree.clockWiseMethod2(new int[][]{
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        }));
+
+        assertArrayEquals(new int[][]{
+                {3, 1},
+                {4, 2}
+        }, RotateMatrix90Degree.clockWiseMethod2(new int[][]{
+                {1, 2},
+                {3, 4}
+        }));
+
+        assertArrayEquals(new int[][]{
+                {1, 2, 3, 4},
+                {5, 6, 7, 8},
+                {9, 10, 11, 12},
+                {13, 14, 12, 16}
+        }, RotateMatrix90Degree.clockWiseMethod2(new int[][]{
                 {4, 8, 12, 16},
                 {3, 7, 11, 12},
                 {2, 6, 10, 14},
