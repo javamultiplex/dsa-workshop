3947c69b3d5c20162f758d89677cf840f7282758
Rohit Agarwal <programmingwithrohit@gmail.com>
2021-03-02 22:50:31 +0530

    code refactor

--- src/main/java/com/javamultiplex/matrix/Main.java
+++
@@ -1,8 +0,0 @@
-package com.javamultiplex.matrix;
-
-/**
- * @author Rohit Agarwal on 01/03/21 11:55 pm
- * @copyright www.javamultiplex.com
- */
-public class Main {
-}
--- src/test/java/com/javamultiplex/matrix/SpiralMatrixTraversalTest.java
+++ src/test/java/com/javamultiplex/matrix/SpiralMatrixTraversalTest.java
@@ -14,13 +14,25 @@ public class SpiralMatrixTraversalTest {
 
     @Test
     public void shouldTraverseSpiralMatrix() {
-        List<Integer> list = SpiralMatrixTraversal.traverse(new int[][]{{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}});
+        List<Integer> list = SpiralMatrixTraversal.traverse(new int[][]
+                {
+                        {1, 2, 3, 4},
+                        {5, 6, 7, 8},
+                        {9, 10, 11, 12},
+                        {13, 14, 15, 16}
+                });
         Assertions.assertEquals(Arrays.asList(1, 2, 3, 4, 8, 12, 16, 15, 14, 13, 9, 5, 6, 7, 11, 10).toString(), list.toString());
     }
 
     @Test
     public void shouldGetKthElementInSpiralMatrix() {
-        int result = SpiralMatrixTraversal.findK(new int[][]{{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}},4,4,6);
+        int result = SpiralMatrixTraversal.findK(new int[][]
+                {
+                        {1, 2, 3, 4},
+                        {5, 6, 7, 8},
+                        {9, 10, 11, 12},
+                        {13, 14, 15, 16}
+                }, 4, 4, 6);
         Assertions.assertEquals(12, result);
     }
 }
