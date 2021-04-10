3709ee8b694fe6027d6cf906058f265e72936eaf
Rohit Agarwal <programmingwithrohit@gmail.com>
2021-04-10 18:53:44 +0530

    Added more problems

---
+++ src/main/java/com/javamultiplex/daily/LeftViewOfBinaryTree.java
@@ -0,0 +1,53 @@
+package com.javamultiplex.daily;
+
+import java.util.ArrayList;
+
+/**
+ * @author Rohit Agarwal on 10/04/21 3:49 pm
+ * @copyright www.javamultiplex.com
+ */
+public class LeftViewOfBinaryTree {
+
+    public static int MAX_LEVEL = 0;
+
+    static class Node {
+        int data;
+        Node left, right;
+
+        Node(int item) {
+            data = item;
+            left = right = null;
+        }
+    }
+
+    public static ArrayList<Integer> leftView(Node root) {
+        ArrayList<Integer> list = new ArrayList<>();
+        leftView(root, list, 1);
+        MAX_LEVEL = 0;
+        return list;
+    }
+
+    private static void leftView(Node root, ArrayList<Integer> list, int level) {
+        if (root == null) {
+            return;
+        }
+        if (MAX_LEVEL < level) {
+            list.add(root.data);
+            MAX_LEVEL = level;
+        }
+
+        leftView(root.left, list, level + 1);
+        leftView(root.right, list, level + 1);
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(4);
+        root.left = new Node(5);
+        root.right = new Node(2);
+        root.right.left = new Node(3);
+        root.right.right = new Node(1);
+        root.right.left.left = new Node(6);
+        root.right.left.right = new Node(7);
+        System.out.println(leftView(root));
+    }
+}
---
+++ src/main/java/com/javamultiplex/daily/MissingNumberInArray.java
@@ -0,0 +1,25 @@
+package com.javamultiplex.daily;
+
+/**
+ * @author Rohit Agarwal on 10/04/21 3:28 pm
+ * @copyright www.javamultiplex.com
+ */
+public class MissingNumberInArray {
+    public static int missingNumber(int[] array, int n) {
+        long expectedSize = (long) n * (n + 1) / 2;
+        long actualSize = 0;
+        int length = array.length;
+        for (int i = 0; i < length; i++) {
+            actualSize += array[i];
+        }
+        return (int) (expectedSize - actualSize);
+    }
+
+
+    public static void main(String[] args) {
+        int missingNumber = missingNumber(new int[]{1, 2, 3, 5}, 5);
+        System.out.println(missingNumber);
+        int missingNumber1 = missingNumber(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 10}, 10);
+        System.out.println(missingNumber1);
+    }
+}
