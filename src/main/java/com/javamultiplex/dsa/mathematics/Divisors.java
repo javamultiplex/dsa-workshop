ebb3d5c018696a0a1096de4b879223d22005edd9
Rohit Agarwal <programmingwithrohit@gmail.com>
2021-04-25 16:22:44 +0530

    Added maths problems

---
+++ src/main/java/com/javamultiplex/dsa/mathematics/Divisors.java
@@ -0,0 +1,68 @@
+package com.javamultiplex.dsa.mathematics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Rohit Agarwal on 25/04/21 3:16 pm
+ * @copyright www.javamultiplex.com
+ */
+//Print all divisors in ascending order
+public class Divisors {
+
+    //TC - theta(n)
+    public static List<Integer> method1(int n) {
+        List<Integer> output = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                output.add(i);
+            }
+        }
+        return output;
+    }
+
+    //TC - theta(sqrt(n)) but result is not in sorted order
+    public static List<Integer> method2(int n) {
+        List<Integer> output = new ArrayList<>();
+        for (int i = 1; i * i <= n; i++) {
+            if (n % i == 0) {
+                output.add(i);
+                if (i != n / i) {
+                    output.add(n / i);
+                }
+            }
+        }
+        return output;
+    }
+
+    //TC - theta(sqrt(n)) but result is in sorted order
+    public static List<Integer> method3(int n) {
+        List<Integer> output = new ArrayList<>();
+        int i;
+        for (i = 1; i * i < n; i++) {
+            if (n % i == 0) {
+                output.add(i);
+            }
+        }
+
+        for (i = (int) Math.sqrt(n); i >= 1; i--) {
+            if (n % i == 0) {
+                output.add(n / i);
+            }
+        }
+        return output;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> list = method1(30);
+        System.out.println(list);
+
+        List<Integer> list1 = method2(30);
+        System.out.println(list1);
+
+        List<Integer> list2 = method3(30);
+        System.out.println(list2);
+    }
+
+
+}
---
+++ src/main/java/com/javamultiplex/dsa/mathematics/PrimeFactors.java
@@ -0,0 +1,104 @@
+package com.javamultiplex.dsa.mathematics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Rohit Agarwal on 25/04/21 2:21 pm
+ * @copyright www.javamultiplex.com
+ */
+public class PrimeFactors {
+    public static List<Integer> method1(int n) {
+        List<Integer> output = new ArrayList<>();
+        //TC - O(n*sqrt(n)*log(n))
+        for (int i = 1; i <= n; i++) {
+            //TC - O(sqrt(n))
+            if (Prime.method3(i)) {
+                int x = i;
+                //TC - O(log(n))
+                while (n % x == 0) {
+                    output.add(i);
+                    x = x * x;
+                }
+            }
+        }
+        return output;
+    }
+
+    public static List<Integer> method2(int n) {
+        List<Integer> output = new ArrayList<>();
+        if (n <= 1) {
+            return output;
+        }
+
+        for (int i = 2; i * i <= n; i++) {
+            while (n % i == 0) {
+                output.add(i);
+                n = n / i;
+            }
+        }
+        if (n > 1) {
+            output.add(n);
+        }
+        return output;
+    }
+
+    public static List<Integer> method3(int n) {
+        List<Integer> output = new ArrayList<>();
+        if (n <= 1) {
+            return output;
+        }
+
+        while (n % 2 == 0) {
+            output.add(2);
+            n = n / 2;
+        }
+
+        while (n % 3 == 0) {
+            output.add(3);
+            n = n / 3;
+        }
+
+        for (int i = 5; i * i <= n; i += 6) {
+            while (n % i == 0) {
+                output.add(i);
+                n = n / i;
+            }
+
+            while (n % (i + 2) == 0) {
+                output.add(i + 2);
+                n = n / (i + 2);
+            }
+        }
+
+        if (n > 3) {
+            output.add(n);
+        }
+        return output;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> list = method1(12);
+        System.out.println(list);
+        List<Integer> list1 = method1(13);
+        System.out.println(list1);
+        List<Integer> list2 = method3(450);
+        System.out.println(list2);
+
+        List<Integer> list3 = method2(12);
+        System.out.println(list3);
+        List<Integer> list4 = method2(13);
+        System.out.println(list4);
+        List<Integer> list5 = method2(450);
+        System.out.println(list5);
+
+        List<Integer> list6 = method3(12);
+        System.out.println(list6);
+        List<Integer> list7 = method3(13);
+        System.out.println(list7);
+        List<Integer> list8 = method3(450);
+        System.out.println(list8);
+
+
+    }
+}
