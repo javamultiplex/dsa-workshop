101243f645c7c32bfc0dac0f5f699ad5333f0a4f
Rohit Agarwal <programmingwithrohit@gmail.com>
2021-06-01 23:49:49 +0530

    Added bitwise problem

--- src/main/java/com/javamultiplex/dsa/bitmagic/GrayToBinaryCodeConverter.java
+++ src/main/java/com/javamultiplex/dsa/bitmagic/GrayToBinaryCodeConverter.java
@@ -7,6 +7,17 @@ package com.javamultiplex.dsa.bitmagic;
 public class GrayToBinaryCodeConverter {
 
     public static int v1(int n) {
-        return 0;
+        int temp = (1 << 31) & n;
+        int res = temp;
+        for (int bit = 30; bit >= 0; bit--) {
+            temp = ((1 << bit) & n) ^ temp;
+            res = res + temp;
+            temp = temp >> 1;
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(v1(0));
     }
 }
